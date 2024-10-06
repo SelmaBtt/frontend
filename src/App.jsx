6fa9a7c2e8d60d442cf5c32d29061f9388cd2ddc
meiyi <meiyinghu18@gmail.com>
2024-10-06 08:34:43 +0200

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,72 @@
 import {
+  Header,
   ContactInfo,
-  ContactLink,
   Filter,
   SearchBar,
-  SearchResult,
-  ShowImages,
-  Popup,
   FetchTe,
   Footer,
 } from "@jennystarkfrk/components";
 import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
-  const teaData = [
-    {
-      id: 1,
-      name: "Grönt te",
-      image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHfamFahLTBWDTLUKhzs6wYtrsZd5HHjmTtA&s",
-    },
-    {
-      id: 2,
-      name: "Svart te",
-      image:
-        "https://www.healthifyme.com/blog/wp-content/uploads/2020/02/Black-Tea-2-1.jpg",
-    },
-    {
-      id: 3,
-      name: "Rött te",
-      image: "https://kahlstkh.se/wp-content/uploads/2018/03/te-rooibos.jpg",
-    },
-    {
-      id: 4,
-      name: "Vitt te",
-      image:
-        "https://sakiproducts.com/cdn/shop/articles/20221107133813-white-tea-recipe-blog_800x800.jpg?v=1667828741",
-    },
-    {
-      id: 5,
-      name: "Jasmin silver needle",
-      image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxmRhyN1GjGMoEycRZfpb9b4ahFBlH_H9Vrg",
-    },
-  ];
+  const [categories, setCategories] = useState([]);
+  const [products, setProducts] = useState([]);
 
-  const productsData = {
-    name: "Jasmine te",
-    image: "/public/Images/teaPlaceholder.png",
-    effect:
-      "hjälper till att lugna, förbättra mental klarhet och stärka ditt immunförsvar",
-    caffeine: "Låg koffein",
-    type: "Grönt, svart & vitt",
-  };
+  // Fetch categories
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const res = await axios.get(
+          "https://storeapi20240917165254.azurewebsites.net/api/category"
+        );
+        setCategories(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchCategories();
+  }, []);
+
+  // Fetch products
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const res = await axios.get(
+          "https://storeapi20240917165254.azurewebsites.net/api/product"
+        );
+        setProducts(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchProducts();
+  }, []);
 
   return (
     <>
-      <ContactInfo />
-      <ContactLink />
-      <SearchBar />
-      <SearchResult /> <br />
-      <ShowImages />
-      <Filter teaData={teaData} />
-      <Popup productsData={productsData} />
-      <FetchTe />
+      <Header />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <SearchBar teaData={products} />
+              <Filter teaData={categories} />
+              <FetchTe sortimentData={products} />
+            </>
+          }
+        />
+        <Route
+          path="/products"
+          element={<FetchTe sortimentData={products} />}
+        />
+        <Route path="/contact" element={<ContactInfo />} />
+      </Routes>
       <Footer />
     </>
   );
